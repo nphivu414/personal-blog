34dccf0d9e30f14408c2441146cb8f7c2e41f5ae
Vu Nguyen <nphivu414@gmail.com>
2020-01-11 15:32:18 +0700

    - Update footer - Config tslint

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,10 +10,10 @@ import {
   ListItemText,
   Avatar,
   Typography,
-  Divider,
 } from '@material-ui/core';
 import { Facebook, Twitter, LinkedIn } from '@material-ui/icons';
 import { getGeneralSetting } from 'src/redux/selectors/setting/settingSelector';
+import LatestProject from 'src/components/Footer/LatestProject';
 
 import useStyles from './style';
 
@@ -28,8 +28,8 @@ const Footer: React.FunctionComponent<FooterProps> = () => {
     <div className={classes.footer}>
       <div className={classes.footerBottom}>
         <Container>
-          <Grid container spacing={10}>
-            <Grid item lg={4}>
+          <Grid container={true} spacing={10}>
+            <Grid item={true} lg={4}>
               <List
                 subheader={<Typography variant="h5">About Me</Typography>}
               >
@@ -47,7 +47,7 @@ const Footer: React.FunctionComponent<FooterProps> = () => {
                 </ListItem>
               </List>
             </Grid>
-            <Grid item lg={2}>
+            <Grid item={true} lg={2}>
               <List
                 subheader={<Typography variant="h5">Follow Me</Typography>}
               >
@@ -58,84 +58,9 @@ const Footer: React.FunctionComponent<FooterProps> = () => {
                 </ListItem>
               </List>
             </Grid>
-            <Grid item lg={6}>
-              <List
-                subheader={<Typography variant="h5">Recent Posts</Typography>}
-              >
-                <ListItem alignItems="flex-start">
-                  <ListItemAvatar>
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="/static/images/avatar/1.jpg"
-                    />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary="Brunch this weekend?"
-                    secondary={
-                      <React.Fragment>
-                        <Typography
-                          component="span"
-                          variant="body2"
-                          color="textPrimary"
-                        >
-                          Ali Connors
-                        </Typography>
-                        {" — I'll be in your neighborhood doing errands this…"}
-                      </React.Fragment>
-                    }
-                  />
-                </ListItem>
-                <Divider variant="inset" component="li" />
-                <ListItem alignItems="flex-start">
-                  <ListItemAvatar>
-                    <Avatar
-                      alt="Travis Howard"
-                      src="/static/images/avatar/2.jpg"
-                    />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary="Summer BBQ"
-                    secondary={
-                      <React.Fragment>
-                        <Typography
-                          component="span"
-                          variant="body2"
-                          color="textPrimary"
-                        >
-                          to Scott, Alex, Jennifer
-                        </Typography>
-                        {" — Wish I could come, but I'm out of town this…"}
-                      </React.Fragment>
-                    }
-                  />
-                </ListItem>
-                <Divider variant="inset" component="li" />
-                <ListItem alignItems="flex-start">
-                  <ListItemAvatar>
-                    <Avatar
-                      alt="Cindy Baker"
-                      src="/static/images/avatar/3.jpg"
-                    />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary="Oui Oui"
-                    secondary={
-                      <React.Fragment>
-                        <Typography
-                          component="span"
-                          variant="body2"
-                          color="textPrimary"
-                        >
-                          Sandra Adams
-                        </Typography>
-                        {' — Do you have Paris recommendations? Have you ever…'}
-                      </React.Fragment>
-                    }
-                  />
-                </ListItem>
-              </List>
+            <Grid item={true} lg={6}>
+              <LatestProject />
             </Grid>
-
           </Grid>
         </Container>
       </div>
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import postReducer from './slices/post/postSlices';
 import settingReducer from './slices/setting/settingSlices';
+import projectReducer from './slices/project/projectSlices';
 
 const rootReducer = combineReducers({
   post: postReducer,
-  setting: settingReducer
+  setting: settingReducer,
+  project: projectReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
